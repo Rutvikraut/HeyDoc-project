25e805622c6d253a6c82af6f001f0420affad928
Rutvik <rutvikraut567@gmail.com>
2025-07-19 22:51:02 +0530

    Added doctor business logic and routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import express from "express"
 import cors from "cors"
 import patientRoutes from "./src/routes/patientRoutes.js"
+import specializationRoutes from "./src/routes/specializationRoutes.js"
+import doctorRoutes from "./src/routes/doctorRoutes.js"
+
+
 
 const app = express()
 app.use(express.json())
@@ -11,6 +15,10 @@ app.get('/',(req,res)=>{
 })
 
 app.use("/api",patientRoutes)
+app.use("/api",specializationRoutes)
+app.use("/api",doctorRoutes)
+
+
 
 app.listen(4000,()=>{
     console.log("Server Running on Port 4000")
